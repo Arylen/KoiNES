1cdc0170512470c94eaa4fb1cd027ac44eeb2ad8
Arylen <177881981+Arylen@users.noreply.github.com>
2026-04-03 10:35:39 -0500

    Add AddressingMode enum

---
+++ KoiNES/Emulation/Data/AddressingMode.cs
@@ -0,0 +1,15 @@
+namespace KoiNES.Emulation.Data;
+
+public enum AddressingMode
+{
+    Implied,
+    Immediate,
+    ZeroPage,
+    ZeroPageX,
+    ZeroPageY,
+    Absolute,
+    AbsoluteX,
+    AbsoluteY,
+    IndirectX,
+    IndirectY,
+}
